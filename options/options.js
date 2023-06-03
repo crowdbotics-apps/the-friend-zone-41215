f98f20dcea16eebca8c4dc08f0849ae560cd3e52
Melissa Hester <mhester88@gmail.com>
2023-06-03 12:03:27 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "the-friend-zone-41215",
-  url: "https://the-friend-zone-41215.botics.co",
-  api: "https://the-friend-zone-41215.botics.co/api/v1"
-}
+  "name": "the-friend-zone-41215",
+  "url": "https://the-friend-zone-41215.botics.co",
+  "api": "https://the-friend-zone-41215.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
